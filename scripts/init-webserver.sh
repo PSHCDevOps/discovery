67924c0cb7060caf87c974f74dd2f0cee380f56c
chinchalinchin <chinchalinchin@gmail.com>
2020-11-20 09:37:32 -0500

    [BRMP-8582] add script to deploy static doc files during pipeline instead of deployment

--- scripts/init-webserver.sh
+++ scripts/init-webserver.sh
@@ -8,8 +8,6 @@ set -e
 #-------------------------------------------------------------------------------
 
 SCRIPT_DIR="$(cd "$(dirname "$([ `readlink "$0"` ] && echo "`readlink "$0"`" || echo "$0")")"; pwd -P)"
-GH_PAGES_BRANCH="gh-pages"
-GH_PAGES_REMOTE="https://github.com/PSHCDevOps/discovery.git"
 
 #-------------------------------------------------------------------------------
 # Web server initialization
@@ -25,47 +23,3 @@ fi
 
 echo "> Collecting Django static files" | tee -a "$LOG_FILE"
 python3 manage.py collectstatic --noinput >>"$LOG_FILE" 2>&1
-
-#-------------------------------------------------------------------------------
-# Copy documentation into static folder
-#-------------------------------------------------------------------------------
-
-# SSH_KEY=$(cat $VCAP_SERVICES | python -c 'import json,sys;print(json.load(sys.stdin)["user-provided"][0]["credentials"])')
-# mkdir /ssh/ 
-# cd /ssh/
-# echo $SSH_KEY > id_rsa
-# ssh-add .id_rsa
-# echo "> Navigating to $SCRIPT_DIR/../app/static"
-# cd "$SCRIPT_DIR/../app/static"
-
-# if [ -d "docs" ] 
-# then
-  #  echo "> Cleaning /docs/ subdirectory"
-  #  rm -r "docs"
-# fi
-
-# echo "> Creating /docs/ subdirectory"
-# mkdir "docs"
-
-# if [ -d "discovery" ]
-# then
-  # echo "> Removing /discovery/ subdirectory"
-  # rm -r "discovery"
-# fi
-
-# Shallow clone to save space
-# echo "> Cloning generated documentation"
-# git clone -b $GH_PAGES_BRANCH --depth 1 --single-branch $GH_PAGES_REMOTE
-
-# echo "> Copying generated index.html into $SCRIPT_DIR/../docs/templates/doc-index.html"
-# cp "discovery/docs/html/index.html" "../docs/templates/doc-index.html"
-# echo "> Moving generated documentation into $SCRIPT_DIR/../app/static/docs"
-# mv "discovery/docs/html"/* "docs"
-
-# echo "cleaning up cloned repo"
-# rm -rf "discovery"
-
-#-------------------------------------------------------------------------------
-
-echo "> Returning to app root directory"
-cd "$SCRIPT_DIR/../app"
