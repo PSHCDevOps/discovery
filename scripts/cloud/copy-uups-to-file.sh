4b7661f1174841f7c3e5da05ba63ba50d2ddd7eb
chinchalinchin <chinchalinchin@gmail.com>
2020-11-20 08:56:13 -0500

    [BRMP-8582] add regex to script to make jsons pretty

--- scripts/cloud/copy-uups-to-file.sh
+++ scripts/cloud/copy-uups-to-file.sh
@@ -1,5 +1,6 @@
 SCRIPT_DIR="$(cd "$(dirname "$([ `readlink "$0"` ] && echo "`readlink "$0"`" || echo "$0")")"; pwd -P)"
 cd $SCRIPT_DIR
 UUPS=$(cf env discovery-web | python -c "import sys,json; print(json.loads(sys.stdin.read()[133:2006])['VCAP_SERVICES']['user-provided'][0]['credentials'])")
-echo $UUPS > uups.json
-cat uups.json
+JSONIFY=$(echo $UUPS | sed "s/'/\"/g")
+echo $JSONIFY > uups.json
+echo $JSONIFY
