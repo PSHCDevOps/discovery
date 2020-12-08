e1e445346b841e28b3679d86828a61d858f46d61
chinchalinchin <chinchalinchin@gmail.com>
2020-12-08 14:08:14 -0500

    merge to master

--- scripts/init-webserver.sh
+++ scripts/init-webserver.sh
@@ -22,41 +22,4 @@ then
 fi
 
 echo "> Collecting Django static files" | tee -a "$LOG_FILE"
-<<<<<<< HEAD
 python3 manage.py collectstatic --noinput >>"$LOG_FILE" 2>&1
-=======
-python3 manage.py collectstatic --noinput >>"$LOG_FILE" 2>&1
-
-#-------------------------------------------------------------------------------
-# Copy documentation into static folder
-#-------------------------------------------------------------------------------
-
-echo "> Navigating to $SCRIPT_DIR/../app/static"
-cd "$SCRIPT_DIR/../app/static"
-
-if [ -d "docs/" ] 
-then
-    echo "> Cleaning /docs/ subdirectory"
-    rm -r "docs"/*
-else
-    echo "> Creating /docs/ subdirectory"
-    mkdir "docs"
-fi
-
-# Shallow clone to save space
-echo "> Cloning generated documentation"
-git clone -b $GH_PAGES_BRANCH --depth 1 --single-branch $GH_PAGES_REMOTE
-
-echo "> Copying generated index.html into $SCRIPT_DIR/../docs/templates/doc-index.html"
-cp "discovery/docs/html/index.html" "../docs/templates/doc-index.html"
-echo "> Moving generated documentation into $SCRIPT_DIR/../app/static/docs"
-mv "discovery/docs/html"/* "docs"
-
-echo "cleaning up cloned repo"
-rm -rf "discovery"
-
-#-------------------------------------------------------------------------------
-
-echo "> Returning to app root directory"
-cd "$SCRIPT_DIR/../app"
->>>>>>> master
