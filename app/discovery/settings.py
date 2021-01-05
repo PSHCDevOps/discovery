5fefd13cd6e60d6c748a06c977d0124c611bf604
pramodganore <pganore1@gmail.com>
2021-01-05 10:33:59 -0500

    BRMP-9281: Adding new csp changes

--- app/discovery/settings.py
+++ app/discovery/settings.py
@@ -397,12 +397,13 @@ REFERRER_POLICY = 'origin'
 CORS_ORIGIN_ALLOW_ALL = True
 
 CSP_DEFAULT_SRC = ("'self'", "'unsafe-eval'")
-CSP_CONNECT_SRC = ("'self'", 'api.gsa.gov', 'calc-dev.app.cloud.gov', 'localhost', 'calc.gsa.gov')
-CSP_SCRIPT_SRC = ("'self'", "'unsafe-inline'", "'unsafe-eval'", 'www.googletagmanager.com', 'www.google-analytics.com', 'dap.digitalgov.gov')
+CSP_CONNECT_SRC = ("'self'", 'api.gsa.gov', 'calc-dev.app.cloud.gov', 'localhost', 'calc.gsa.gov', 'google-analytics.com')
+CSP_SCRIPT_SRC = ("'self'", "'unsafe-inline'", "'unsafe-eval'", 'googletagmanager.com', 'ssl/google-analytics.com','google-analytics.com', 'dap.digitalgov.gov')
 CSP_STYLE_SRC = ("'self'", "'unsafe-inline'", "fonts.googleapis.com")
-CSP_IMG_SRC = ("'self'", 'www.google-analytics.com')
+CSP_IMG_SRC = ("'self'", 'google-analytics.com')
 CSP_FONT_SRC = ("'self'", 'fonts.gstatic.com')
 CSP_OBJECT_SRC = ("'none'",)
+
 
 SECURE_BROWSER_XSS_FILTER = True
 
